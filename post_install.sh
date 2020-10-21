1174f1813e401022923978f045fdc80a35e89ff9
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2020-10-21 18:21:17 +0000

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -19,3 +19,15 @@ fi
 apt install openssh-server
 systemctl enable openssh-server
 systemctl start openssh-server
+ssh-keygen
+
+echo "Do you want to copy your key to remote server? [y/n]"
+read copy
+if [ $copy = "y" ]
+then
+  echo "Type in the server address you want to copy your key to: "
+  read address
+  echo "Type in the username for $address: "
+  read username
+  cat ~/.ssh/id_rsa.pub | ssh $username@$address 'cat >> .ssh/authorized_keys && echo "Key copied"'
+fi
